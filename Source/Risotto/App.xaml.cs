97042dd3da97b0e253a84c85a5621f564b95d281
Christoph Wille <christophw@alphasierrapapa.com>
2012-11-28 10:21:06 +0100

    Search startup using same code path as protocol activation

--- Source/Risotto/App.xaml.cs
+++ Source/Risotto/App.xaml.cs
@@ -246,40 +246,9 @@ namespace Risotto
             // TODO: Register the Windows.ApplicationModel.Search.SearchPane.GetForCurrentView().QuerySubmitted
             // event in OnWindowCreated to speed up searches once the application is already running
 
-            // If the Window isn't already using Frame navigation, insert our own Frame
-            var previousContent = Window.Current.Content;
-            var frame = previousContent as Frame;
+            await EnsureRootFrameInitialization(args.PreviousExecutionState, Window.Current.Content as Frame);
 
-            // If the app does not contain a top-level frame, it is possible that this 
-            // is the initial launch of the app. Typically this method and OnLaunched 
-            // in App.xaml.cs can call a common method.
-            if (frame == null)
-            {
-                // Create a Frame to act as the navigation context and associate it with
-                // a SuspensionManager key
-                frame = new Frame();
-                NavigationService.Initialize(frame);
-                SuspensionManager.RegisterFrame(frame, "AppFrame");
-
-                if (args.PreviousExecutionState == ApplicationExecutionState.Terminated)
-                {
-                    // Restore the saved session state only when appropriate
-                    try
-                    {
-                        await SuspensionManager.RestoreAsync();
-                    }
-                    catch (SuspensionManagerException)
-                    {
-                        //Something went wrong restoring state.
-                        //Assume there is no state and continue
-                    }
-                }
-            }
-
-            frame.Navigate(typeof(SearchResultsPage), args.QueryText);
-            Window.Current.Content = frame;
-
-            // Ensure the current window is active
+            NavigationService.Navigate<SearchResultsPage>(args.QueryText);
             Window.Current.Activate();
         }
     }
